1d2e7061d67d6c9e5aed3336b3073f16923913f7
ericksonlopes <ofc.erickson@gmail.com>
2026-08-15 10:27:47 -0300

    remove DiarizationTaskModel from exports in __init__.py

--- src/core/database/__init__.py
+++ src/core/database/__init__.py
@@ -1,4 +1,3 @@
 from src.core.database.connector import Base, engine, Session, ConnectorPostgres
-from src.modules.diarization.infrastructure.repositories.models.diarization_task_model import DiarizationTaskModel
 
-__all__ = ["Base", "engine", "Session", "ConnectorPostgres", "DiarizationTaskModel"]
+__all__ = ["Base", "engine", "Session", "ConnectorPostgres"]
